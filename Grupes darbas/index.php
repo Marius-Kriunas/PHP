9c50b7cb1dabee09ef5b4b6d85970dba950c7d01
Marius-Kriunas <39553628+Marius-Kriunas@users.noreply.github.com>
2018-08-28 12:08:24 +0300

    Create index.php

---
+++ Grupes darbas/index.php
@@ -0,0 +1,23 @@
+<?php
+$mode='Dėstytojų sąrašas';
+$uri = 'http://localhost/PHP/tema10/';
+include 'header.php';
+
+// $sql = "SELECT Vardas, Pavarde FROM destytoju_sarasas LIMIT 20";
+// $result = mysqli_query($conn, $sql);
+
+// if (mysqli_num_rows($result) > 0) {
+//    // kiekvieną eilutę atskirai
+//    while($row = mysqli_fetch_assoc($result)) {
+//        echo "Name: " . $row["Vardas"]. " " . $row["Pavarde"]. "<br>";
+//    }
+// } else {
+//    echo "0 results";
+// }
+
+
+
+
+
+
+include 'footer.php';
