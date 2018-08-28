f3964d2d9c89a3343a9264cfb067987651bb5059
Marius-Kriunas <39553628+Marius-Kriunas@users.noreply.github.com>
2018-08-28 15:33:42 +0300

    Update index.php

--- tema11/index.php
+++ tema11/index.php
@@ -10,5 +10,24 @@ include 'class/Builder.php';
 $programmer = new Programmer();
 $builder = new Builder();
 
+// $programmer->printInfo();
+// $builder->printInfo();
+
 $programmer->printInfo();
+$programmer->addSkill('Rusty');
+$programmer->addSkill('PHP');
+$programmer->addSkill('Pascal');
+$programmer->addSkill('Go');
+$programmer->addSkill('JAVA');
+$programmer->addSkill('Phyton');
+$programmer->printInfo();
+$programmer->bankrupt();
+// $programmer->addSkill('PHP');
+// $programmer->printInfo();
+echo '<br>';
 $builder->printInfo();
+$builder->addSkill('Truck Driver');
+$builder->printInfo();
+$builder->bankrupt();
+// $builder->addSkill('Tank Driver');
+// $builder->printInfo();
