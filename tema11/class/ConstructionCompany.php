d7002fb45645da296e1bb72736dba87ecb282443
Marius-Kriunas <39553628+Marius-Kriunas@users.noreply.github.com>
2018-08-28 12:11:49 +0300

    Create ConstructionCompany.php

---
+++ tema11/class/ConstructionCompany.php
@@ -0,0 +1,15 @@
+<?php
+
+class ConstructionCompany extends Company {
+    protected $buildingObjects;
+    private $build = ['Houses', 'Bridges', 'Offices', 'Roads', 'Gardens', 'Railroads', 'Canals', 'Aqueduct', 'Stadions'];
+
+    public function __construct() {
+        parent::__construct();
+        $this->buildingObjects = (array)array_rand(array_flip($this->build), 4);
+    }
+
+    protected function printInfoConstructionCompany() {
+        echo 'Building Objects: '.implode(' - ',$this->buildingObjects).'<br>';
+    }
+    }
